a14d229541e3f1a357366f4dc8464f7c146c9cef
Jack Ratner <jackratner@gmail.com>
2025-05-22 15:53:37 -0400

    wtf there's a part 2!?

--- 2017-1.clj
+++ 2017-1.clj
@@ -1,6 +1,6 @@
 #!/usr/bin/env bb
 
-(defn captcha-sum [num-str]
+(defn captcha-sum-next [num-str]
   (let [list (vec (map #(Character/digit % 10) num-str))
         summable-val
         (fn [i v]
@@ -10,12 +10,27 @@
             (if (= v n) v 0)))]
     (reduce + (map-indexed summable-val list))))
 
-(assert (= (captcha-sum "1122") 3))
-(assert (= (captcha-sum "1111") 4))
-(assert (= (captcha-sum "1234") 0))
-(assert (= (captcha-sum "91212129") 9))
+(assert (= (captcha-sum-next "1122") 3))
+(assert (= (captcha-sum-next "1111") 4))
+(assert (= (captcha-sum-next "1234") 0))
+(assert (= (captcha-sum-next "91212129") 9))
+
+(defn captcha-sum-rot [num-str]
+  (let [list (vec (map #(Character/digit % 10) num-str))
+        list-len (count list)
+        summable-val
+        (fn [i v]
+          (let [n (get list (mod (+ i (/ list-len 2)) list-len))]
+            (if (= v n) v 0)))]
+    (reduce + (map-indexed summable-val list))))
+
+(assert (= (captcha-sum-rot "1212") 6))
+(assert (= (captcha-sum-rot "1221") 0))
+(assert (= (captcha-sum-rot "123425") 4))
+(assert (= (captcha-sum-rot "123123") 12))
+(assert (= (captcha-sum-rot "12131415") 4))
 
 (let [[nums] *command-line-args*]
   (->> nums
-       captcha-sum
+       captcha-sum-rot
        println))
